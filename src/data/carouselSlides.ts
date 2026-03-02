b0874305a51ee59eb43274b838cfbca599d28f83
mgjeffries <gib.jeffries@gmail.com>
2026-03-01 18:35:21 -0600

    update about us images

--- src/data/carouselSlides.ts
+++ src/data/carouselSlides.ts
@@ -1,6 +1,7 @@
 export const aboutSlides = [
-  { src: '/images/about-us/sunset-bridge.jpg', alt: 'Sunset view from a bridge' },
+  { src: '/images/about-us/2025-07-05-erin-gib-bike-highway.JPG', alt: 'Erin and Gib on the US 36 Bikeway' },
   { src: '/images/about-us/denver-pedestrian-bridge.JPG', alt: 'Pedestrian bridge in Denver' },
+  { src: '/images/about-us/erin-on-tiny-greenway.JPG', alt: 'Erin on a tiny greenway' },
   { src: '/images/about-us/30a-bike-gib.JPG', alt: 'Biking on 30A' },
-  { src: '/images/about-us/boyscout-patch.png', alt: 'Boy Scout patch' },
+  { src: '/images/about-us/sunset-bridge.jpg', alt: 'Sunset view from a bridge' },
 ]
