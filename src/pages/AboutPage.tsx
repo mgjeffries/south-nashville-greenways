34f17c11e5ef3e2089e5a86795c0dac3db136196
mgjeffries <gib.jeffries@gmail.com>
2026-03-01 18:41:19 -0600

    Use 3rd person

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -14,7 +14,7 @@ export default function AboutPage() {
             <p>South Nashville deserves a connected network of greenways where families can ride safely, where neighbors can leave their house without a car, and where our community can come together in person. This page, this organization, this vision—it's all just getting started.
             If you believe in creating better, safer, more connected spaces for cycling in South Nashville, I'd love for you to be part of this. Whether you're a lifelong cyclist or someone who's just curious about what greenways could mean for our community, there's a place for you here.
             Let's build this together.</p>
-            <p>If you want to connect, reach out to me at gib.jeffries@gmail.com</p>
+            <p>If you want to connect, reach out to Gib at gib.jeffries@gmail.com</p>
           </div>
 
           <Carousel slides={aboutSlides} />
