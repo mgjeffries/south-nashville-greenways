dc0e049c8706df65138bfa9268df21e8434ffb91
mgjeffries <gib.jeffries@gmail.com>
2026-03-09 20:20:24 -0500

    update post

--- src/data/posts/antioch-nolensville-fatality.ts
+++ src/data/posts/antioch-nolensville-fatality.ts
@@ -12,8 +12,12 @@ const post: BlogPost = {
       includeOnCoverCard: true,
     },
     {
+        src: '/images/blog/antioch-nolensville-fatality/crosswalk.png',
+        alt: 'The crosswalk markings at the intersection of Antioch Pike and Nolensville Pike are mostly missing',
+    },
+    {
       src: '/images/blog/antioch-nolensville-fatality/overhead.png',
-      alt: 'Overhead view from Google Maps showing a painted median reducing turning traffic onto Antioch Pike to one lane',
+      alt: 'Overhead view from Google Maps showing a painted median',
     },
     {
       src: '/images/blog/antioch-nolensville-fatality/patch.png',
@@ -28,12 +32,12 @@ const post: BlogPost = {
     'A pedestrian was struck and killed on Antioch Pike on February 20th. A visit to the intersection reveals missing crosswalk markings and an eroded painted median that need urgent repair.',
   content: [
     'A pedestrian was struck and killed by a vehicle on Antioch Pike on February 20th. Not knowing this had occurred, I biked through the intersection two days later while recording my ride.',
-    { type: 'image', imageIndex: 0 },
+    { type: 'image', imageIndex: 1 },
     'As I approached the intersection from Nolensville Pike heading East, I noticed that the painted crosswalks are mostly missing. I would like to see it re-painted to increase awareness of pedestrians.',
-    { type: 'image', imageIndex: 1, caption: 'An overhead image from Google Maps captured in 2026 shows a painted median reducing the traffic turning onto Antioch Pike from Nolensville down to one lane.' },
-    'But when I turned onto the road it seems that this painted median has already been eroded since the Google image was taken. A patch has replaced the leading edge of the median, and the following portion is also missing.',
-    { type: 'image', imageIndex: 2 },
-    { type: 'image', imageIndex: 3 },
+    { type: 'image', imageIndex: 2, caption: 'Google maps 2026' },
+    'An overhead image from Google Maps captured in 2026 shows a painted median reducing the traffic turning onto Antioch Pike from Nolensville down to one lane. But when I turned onto the road it seems that this painted median has already been eroded since the Google image was taken. A patch has replaced the leading edge of the median, and the following portion is also missing.',
+    { type: 'image', imageIndex: 3, caption: "Patch covers painted median" },
+    { type: 'image', imageIndex: 4, caption: "Still no sign of the median" },
     'I hope that Nashville will act quickly to repair these issues and protect people walking and biking in the city.',
   ],
 }
