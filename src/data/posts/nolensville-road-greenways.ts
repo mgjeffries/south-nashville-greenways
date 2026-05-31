b623c99be9c88c67a43f1b1d93ee8e67624901e0
mgjeffries <gib.jeffries@gmail.com>
2026-05-31 08:48:20 -0500

    add table of contents and heading

--- src/data/posts/nolensville-road-greenways.ts
+++ src/data/posts/nolensville-road-greenways.ts
@@ -8,6 +8,7 @@ const post: BlogPost = {
   summary:
     "We've got traffic jams! We've got crackle and pop exhausts! We've got drag racing! But even with all that we have an opportunity to add safe, protected paths for walking and biking on Nolensville road. The type of path you can walk on with your family as you head from your favorite local shop to Coleman Park!",
   content: [
+    { type: "tableOfContents" },
     { type: 'heading', level: 2, text: 'Oh, Really?' },
     "The stretch of Nolensville pike has some challenges for walking and biking. There are sidewalks, which is great, but they are close to the highway. They are great for getting from one business to the next, but not very pleasant for an extended walk. Also, there are no bike lanes on this section of the road, and although a tiny sign declares \"NOTICE BIKE MAY USE FULL LANE\" it would be extremely dangerous to any bike rider who attempts this.",
     { type: 'image', src: '/images/blog/nolensville-road-greenways/bike-may-use-full-lane.png', alt: 'Sign reading "Notice Bike May Use Full Lane" on Nolensville Pike' },
@@ -19,6 +20,7 @@ const post: BlogPost = {
     { type: 'image-comparison', left: { src: '/images/blog/nolensville-road-greenways/compare-left.png', alt: 'Current state of the alley on the east side of Nolensville Pike' }, right: { src: '/images/blog/nolensville-road-greenways/compare-right.png', alt: 'Conceptual rendering of a greenway on the east side of Nolensville Pike' } },
     { type: 'heading', level: 2, text: 'Friendly Debate' },
     "Check out the interactive map and see what you think! If you click on the 📷 icons you can see a conecptual rendering at that location, as well as current image for comparison. Would you use a path like this if it existed? Do you like it? Do you hate it? Do you own property or a business that would be affected by this for better or worse?",
+    { type: 'heading', level: 3, text: 'Interactive Map' },
     { type: 'iframe', src: 'https://www.google.com/maps/d/u/0/embed?mid=1_3ZQLIKoZxqcnogii7rtehGfVXHL0ww&ehbc=2E312F&noprof=1', width: 640, height: 480, title: 'Nolensville Road Greenways Map' },
     { type: 'callout', text: "I'd love to hear your thoughts about this pitch! Please send me an email with your feedback, or even just a note saying that you'd like to hear more updates!" },
   ],
